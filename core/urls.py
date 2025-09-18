b0bba70b5ae0a56e2c0f2d3ce8064782a850771b
evann <evann.megret@gmail.com>
2025-09-18 19:24:08 +0200

    feat: add location page and enhance navigation with mega menu

--- core/urls.py
+++ core/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('produits/', views.product_list, name='product_list'),
     path('produits/<slug:slug>/', views.product_detail, name='product_detail'),
     path('preview/<slug:slug>/', views.product_preview, name='product_preview'),
+    path('localisation/', views.location, name='location'),
 ]
